c77c94d6e0750023048f77b3ce62e0a48773f658
00001093171030 <00001093171030sp@al.educacao.sp.gov.br>
2024-11-22 15:22:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animais',
+    'Qual é o animal mais rápido do mundo?',
+    'O animal mais rápido do mundo é o falcão-peregrino, que pode atingir velocidades de até 390 km/h durante seus mergulhos para capturar presas. Em terra, o guepardo é o mais veloz, correndo até 120 km/h em curtas distâncias.'
+)
+
+criaCartao(
+    'Animais',
+    'Por que os camaleões mudam de cor?',
+    'Os camaleões mudam de cor para se camuflar, regular a temperatura corporal e comunicar emoções. Essa mudança ocorre por meio de células especiais na pele chamadas cromatóforos, que reorganizam pigmentos para refletir diferentes cores.'
+)
+
+criaCartao(
+    'Animais',
+    'Como as baleias respiram debaixo da água?',
+    'As baleias não respiram debaixo da água, pois são mamíferos e precisam subir à superfície para respirar através de suas narinas, chamadas espiráculos. Elas podem prender a respiração por longos períodos, dependendo da espécie algumas chegam a 90 minutos'
+)
+
+criaCartao(
+    'Animais',
+    'Por que os elefantes têm orelhas tão grandes?',
+    'As orelhas grandes dos elefantes ajudam a regular a temperatura corporal. Quando estão quentes, eles abanam as orelhas para resfriar o sangue que passa por grandes vasos sanguíneos nessa região, ajudando a reduzir o calor do corpo.'
+)
